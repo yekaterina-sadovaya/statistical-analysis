0c66491a6f78168149dfd5771dd4b7aa0cc8c5a0
Yekaterina Sadovaya <sadovaya.ek@gmail.com>
2022-09-13 13:26:36 +0300

    change second task

--- 02 Mixed Models/Children.R
+++ 02 Mixed Models/Children.R
@@ -7,14 +7,14 @@ head(data)
 tail(data)
 interaction.plot(ID,gender,height)
 # a
-model<-lmer(height~age + poly(age, degree=2) + poly(age, degree=3) + (1+ID|gender), data=data)
+model<-lmer(height~(age + poly(age, degree=2) + poly(age, degree=3))*gender + (1+gender|ID), data=data)
 summary(model)
 # b
 ranef(model)
-ranef(model)$ID[[93]]
+ranef(model)$ID[[93, 2]]
 # c
-model.H0<-lmer(height~age + poly(age, degree=2) + poly(age, degree=3) + (1+ID|gender), data=data, REML=FALSE)
-model.H1<-lmer(height~(age + poly(age, degree=2) + poly(age, degree=3))*gender + (1+ID|gender), data=data, REML=FALSE)
+model.H0<-lmer(height~age + poly(age, degree=2) + poly(age, degree=3) + (1+gender|ID), data=data, REML=FALSE)
+model.H1<-lmer(height~(age + poly(age, degree=2) + poly(age, degree=3))*gender + (1+gender|ID), data=data, REML=FALSE)
 anova(model.H0, model.H1)
 anova(model.H0, model.H1)$Chisq[2]
 # d
@@ -24,4 +24,25 @@ mu.pred
 
 rm(list = ls())
 data<-read.table("treemoisture.txt", header=TRUE, sep="\t", dec=".")
-
+x1=data$Location
+x2=data$Species
+x3=data$Transpiration
+y=data$Moisture
+z=data$Branch.ID
+model<-lmer(y~(x1+x2+x3)*z + (1|z), data=data)
+summary(model)
+# a
+coef(model)
+# b
+ranef(model)
+# c
+sigma2<-sigma(model)^2
+sigma2
+# d
+model.H0<-lmer(y~(x1+x2)*z + (1|z), data=data, REML=FALSE)
+model.H1<-lmer(y~(x1+x2+x3)*z + (1|z), data=data, REML=FALSE)
+anova(model.H0, model.H1)
+# e
+newdata<-expand.grid(x1='Central', x2='Yellow Poplar', x3='Slow', z=15)
+mu.pred<-predict(model, newdata=newdata)
+mu.pred
