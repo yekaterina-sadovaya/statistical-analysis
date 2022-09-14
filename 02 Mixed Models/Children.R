915bea980d8eb16a4cbd765e95bb7d64f90e7fc8
Yekaterina Sadovaya <sadovaya.ek@gmail.com>
2022-09-14 16:10:46 +0300

    add AIC selection

--- 02 Mixed Models/Children.R
+++ 02 Mixed Models/Children.R
@@ -29,14 +29,30 @@ x2=data$Species
 x3=data$Transpiration
 y=data$Moisture
 z=data$Branch.ID
-model<-lmer(y~(x1+x2+x3)*z + (1|z), data=data)
-summary(model)
+# make 3 different models
+m1<-aov(y~x1*x2*x3*z, data=data)
+m2<-aov(y~(x1+x2+x3)*z, data=data)
+m3<-aov(y~x1*x2*x3, data=data)
+# test these models with the Akaike information criterion
+AIC(m1, m2, m3)
+# use the one, which gave the best score
+model_mixed_eff<-lmer(y~x1*x2*x3 + (1|z), data=data)
+summary(model_mixed_eff)
 # a
-coef(model)
+# all estimation for mu
+mu_all = model_mixed_eff@resp$mu
+# next, we need to specify the indices where x1 = Central, x2 = Yellow Poplar, 
+# and xi∗3 = Slow
+sub_data = data[which(data$Location == "Central" & data$Species == "Yellow Poplar" 
+           & data$Transpiration == "Slow"), ]
+indexes_string = row.names(sub_data)
+indexes = as.numeric(unlist(strsplit(indexes_string, ",", fixed=TRUE)))
+# finally, we can obtain the estimate for the required values
+mu_all[indexes]
 # b
-ranef(model)
+ranef(model_mixed_eff)$z[10,1]
 # c
-sigma2<-sigma(model)^2
+sigma2<-sigma(model_mixed_eff)^2
 sigma2
 # d
 model.H0<-lmer(y~(x1+x2)*z + (1|z), data=data, REML=FALSE)
@@ -46,3 +62,10 @@ anova(model.H0, model.H1)
 newdata<-expand.grid(x1='Central', x2='Yellow Poplar', x3='Slow', z=15)
 mu.pred<-predict(model, newdata=newdata)
 mu.pred
+
+
+rm(list = ls())
+data<-read.table("foreststage.txt", sep="\t", dec=".", header=TRUE)
+model<-lmer(height.m~dbhib.cm+I(dbhib.cm^2)+(1+dbhib.cm|ID)+(1|Forest.ID), data=data)
+summary(model)
+v <- VarCorr(model)
